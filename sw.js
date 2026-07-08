7ae18336d2d28e7a2d5c1abb9bc398365c76a19f
LOFTHALL <lofthall@local>
2026-07-08 14:26:39 +0300

    Build Ravian MVP

--- sw.js
+++ sw.js
@@ -1,8 +1,16 @@
-const CACHE_NAME = "finora-v1";
+const CACHE_NAME = "ravian-v2";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
 });
 
 self.addEventListener("fetch", event => {
