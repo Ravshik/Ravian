d6f2139b4fcf85cb761df322cee8cfc86412dfd0
Ravshik <ravshik@example.com>
2026-07-16 19:14:19 +0300

    Rework Ravian goal tracker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ravian-v2";
+const CACHE_NAME = "ravian-v3";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
